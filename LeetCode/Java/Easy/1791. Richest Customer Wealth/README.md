2c793c4a231b05b4b3b15363b7cc6a4674cda664
Naga purnima tatikonda <tatikondapurnima@gmail.com>
2026-08-27 18:22:42 +0530

    Add Easy Java Solution for 1791. Richest Customer Wealth from LeetCode

--- LeetCode/Java/Easy/1791. Richest Customer Wealth/README.md
+++ LeetCode/Java/Easy/1791. Richest Customer Wealth/README.md
@@ -9,7 +9,7 @@ Array, Matrix
 
 ### 🚀 Performance
 - **Runtime:** 0 ms
-- **Memory:** 44.9 MB
+- **Memory:** 44.7 MB
 
 ---
 
--- LeetCode/Patterns/Matrix/1791. Richest Customer Wealth/Solution.java
+++ LeetCode/Patterns/Matrix/1791. Richest Customer Wealth/Solution.java
@@ -1,18 +1,18 @@
 class Solution {
     public int maximumWealth(int[][] accounts) {
         int max=0;
-        for(int i=0;i<accounts.length;i++)
+    for(int i=0;i<accounts.length;i++)
+    {
+        int sum=0;
+        for(int j=0;j<accounts[i].length;j++)
         {
-            int sum=0;
-            for(int j=0;j<accounts[i].length;j++)
-            {
-                sum+=accounts[i][j];
-            }
-            if(sum>max)
-            {
-                max=sum;
-            }
+            sum+=accounts[i][j];
         }
-        return max;
+       if(sum>max)
+       {
+        max=sum;
+       }
+    }
+    return max;
     }
 }
