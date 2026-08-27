81a5297511f925ee38dc74f18e1424c6d111b584
Naga purnima tatikonda <tatikondapurnima@gmail.com>
2026-08-27 18:28:00 +0530

    Add Easy Java Solution for 1421. Find Numbers with Even Number of Digits from LeetCode

--- LeetCode/Patterns/Arrays/1421. Find Numbers with Even Number of Digits/Solution.java
+++ LeetCode/Patterns/Arrays/1421. Find Numbers with Even Number of Digits/Solution.java
@@ -1,20 +1,21 @@
 class Solution {
     public int findNumbers(int[] nums) {
-     int count=0;
-     int ans=0;
-     for(int i=0;i<nums.length;i++)
-     {
-        int temp=nums[i];
-        while(temp>0)
+        int ans=0;
+        for(int i=0;i<nums.length;i++)
         {
-            count++;
-            temp/=10;
+            int count=0;
+            int temp=nums[i];
+            while(temp>0)
+            {
+                count++;
+                temp/=10;
+            }
+        if(count%2==0)
+        {
+            ans++;
         }
-     }
-     if(count%2==0)
-     {
-         ans++;;
     }
     return ans;
     }
+
 }
