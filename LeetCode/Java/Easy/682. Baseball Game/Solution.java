cafe41119402b0748fd4f8784b4fd6d30820b342
Naga purnima tatikonda <tatikondapurnima@gmail.com>
2026-09-08 14:28:56 +0530

    Add Easy Java Solution for 682. Baseball Game from LeetCode

---
+++ LeetCode/Java/Easy/682. Baseball Game/Solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int calPoints(String[] operations) {
+        Stack<Integer> st = new Stack<>();
+        for (String op : operations) {
+            if (op.equals("C")) {
+                st.pop();
+            }
+            else if (op.equals("D")) {
+                st.push(st.peek() * 2);
+            }
+            else if (op.equals("+")) {
+                int last = st.pop();
+                int secondlast = st.peek();
+                st.push(last);
+                st.push(last + secondlast);
+            }
+            else {
+                st.push(Integer.parseInt(op));
+            }
+        }
+        int sum = 0;
+        while (!st.isEmpty()) {
+            sum += st.pop();
+        }
+        return sum;
+    }
+}
