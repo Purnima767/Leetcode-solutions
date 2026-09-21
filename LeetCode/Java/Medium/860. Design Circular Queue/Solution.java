220845c1fbb1414e83d6537cb50be7f39ec7b038
Naga purnima tatikonda <tatikondapurnima@gmail.com>
2026-09-21 14:49:07 +0530

    Add Medium Java Solution for 860. Design Circular Queue from LeetCode

---
+++ LeetCode/Java/Medium/860. Design Circular Queue/Solution.java
@@ -0,0 +1,55 @@
+class MyCircularQueue {
+    int[] q;
+    int capacity;
+    int front;
+    int rear;
+    int size;
+    public MyCircularQueue(int k) {
+        capacity = k;
+        q = new int[capacity];
+        front = 0;
+        rear = 0;
+        size = 0;
+    }
+
+    public boolean enQueue(int value) {
+        if (isFull()) {
+            return false;
+        }
+        q[rear] = value;
+        rear = (rear + 1) % capacity;
+        size++;
+    }
+
+    public boolean deQueue() {
+        if (isEmpty()) {
+            return false;
+        }
+        front = (front + 1) % capacity;
+        size--;
+    }
+
+    public int Front() {
+        if (isEmpty()) {
+            return -1;
+        }
+
+        return q[front];
+    }
+
+    public int Rear() {
+        if (isEmpty()) {
+            return -1;
+        }
+
+        return q[(rear - 1 + capacity) % capacity];
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public boolean isFull() {
+        return size == capacity;
+    }
+}
